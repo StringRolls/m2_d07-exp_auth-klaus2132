3ce89e587ad5335b54797f38601d3567db0bdf23
Klaus Haugnaess <klaushaugness@gmail.com>
2022-05-10 13:14:54 +0200

    exersie about auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,41 +3,77 @@
 const { Router } = require("express");
 const router = new Router();
 
-const bcryptjs = require("bcryptjs");
-const saltRounds = 10;
+const bcrypt = require("bcrypt");
+const saltRounds = 5;
 
 const User = require("../models/User.model");
+const res = require("express/lib/response");
 
 // GET route ==> to display the signup form to users
-router.get("/signup", (req, res) => res.render("auth/signup"));
+router
+  .route("/signup")
+  .get((req, res) => res.render("auth/signup"))
+  // POST route ==> to process form data
+  .post((req, res, next) => {
+    // console.log("The form data: ", req.body);
 
-// POST route ==> to process form data
-router.post("/signup", (req, res, next) => {
-  // console.log("The form data: ", req.body);
+    const { username, email, password } = req.body;
+    /*
+    if (!username || !email || !password){
+      res.render("auth/login", {errorMessage: "All fields required"})
+      return
+    } // After this line I KNOW i have username, email and password that are NOT "undefined"
+    
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+    if(regex.test(password)){
+      res.render("auth/login", {errorMessage: "Password must follow guidelines"})
+      return
+    }
+*/
+    bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hashedPassword) => {
+        return User.create({
+          // username: username
+          username,
+          email,
+          // passwordHash => this is the key from the User model
+          //     ^
+          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+          passwordHash: hashedPassword,
+        });
+      })
+      .then((userFromDB) => {
+        // console.log("Newly created user is: ", userFromDB);
+        res.redirect("/");
+      })
+      .catch((err) => res.render("auth/signup", { errorMessage: err.message }))
+      .catch((error) => next(error));
+  });
 
-  const { username, email, password } = req.body;
+router
+  .route("/login")
+  .get((req, res) => res.render("auth/login"))
+  .post((req, res) => {
+    const { email, password } = req.body;
 
-  bcryptjs
-    .genSalt(saltRounds)
-    .then((salt) => bcryptjs.hash(password, salt))
-    .then((hashedPassword) => {
-      return User.create({
-        // username: username
-        username,
-        email,
-        // passwordHash => this is the key from the User model
-        //     ^
-        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
-      });
-    })
-    .then((userFromDB) => {
-      // console.log("Newly created user is: ", userFromDB);
-      res.redirect("/userProfile");
-    })
-    .catch((error) => next(error));
-});
-
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
-
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          res.render("auth/login", { errorMessage: "Wrong credentials!" });
+          return;
+        } else {
+          if (bcrypt.compareSync(password, user.passwordHash)) {
+            req.session.currentUser = user;
+            res.redirect("/userProfile");
+            return;
+          } else {
+            res.render("auth/login", { errorMessage: "Wrong credentials!" });
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+  
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
+const { isLoggedin } = require("../middelwares/auth.middelwares");
+
+router.get("/userProfile", isLoggedin, (req, res) =>
+  res.render("users/user-profile", {user: req.session.currentUser})
+);
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
